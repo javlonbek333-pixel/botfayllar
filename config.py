27c66833c6e08266fce6503de498cb3029b4c301
javlonbek333-pixel <javlonbek333@gmail.com>
2026-09-19 20:22:32 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 import os
 
-# Railway Environment Variables’dan olinadi, lokalda ishlatsangiz ikkinchi qiymatga yozasiz
-BOT_TOKEN = os.getenv("BOT_TOKEN", "YOUR_LOCAL_BOT_TOKEN")
-AUDD_API_KEY = os.getenv("AUDD_API_KEY", "YOUR_LOCAL_AUDD_KEY")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+AUDD_API_KEY = os.getenv("AUDD_API_KEY")
 
 TEMP_DIR = "temp"
